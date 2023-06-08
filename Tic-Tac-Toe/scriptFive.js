cc800dac3f9db6f7ef8d6858bb27e75f1c83452c
theoliverlear <sigwartholiver2@gmail.com>
2023-06-08 17:31:33 -0500

    Full Project v1.0
    Completed a draw condition which works in tandem with the other win conditions. Moved images to image folder and made small CSS adjustments.

--- Tic-Tac-Toe/scriptFive.js
+++ Tic-Tac-Toe/scriptFive.js
@@ -2,11 +2,6 @@ document.addEventListener('DOMContentLoaded', function () {
     // Image URL
     const OSrc = "https://github.com/UWRF-Development-Team/Games-Website/blob/4e1b493e0f8263df59a733d7b9947867f0456d72/Tic-Tac-Toe/O.png?raw=true";
     const XSrc = "https://github.com/UWRF-Development-Team/Games-Website/blob/4e1b493e0f8263df59a733d7b9947867f0456d72/Tic-Tac-Toe/X.png?raw=true";
-    // Image elements
-    const O = new Image();
-    const X = new Image();
-    O.src = OSrc;
-    X.src = XSrc;
     const startButton = document.getElementById("start-button");
     const notificationButton = document.getElementById("announcement");
     let spotOne = document.getElementById("one");
@@ -21,14 +16,19 @@ document.addEventListener('DOMContentLoaded', function () {
     let board = [spotOne, spotTwo, spotThree, spotFour, spotFive, spotSix, spotSeven, spotEight, spotNine];
     let currentPlayer  = 1;
     let isGoing = true; //variable to tell if the game is still going.
-
-    for (let item of board) { //display all items in board array
-        console.log(item);
+    
+    function isDraw() {
+        let count = 0;
+        board.forEach(e => {if (e.style.opacity === '1') count++;});
+        return count === 9;
     }
-
-    console.log(OSrc); //testing
-    console.log(XSrc);
-
+    function announceDraw() {
+        notificationButton.innerText = "A Draw!";
+        freezeBoard();
+        isGoing = false;
+        startButton.innerText = "Play Again";
+    }
+    
     for (let item of board) {
         item.addEventListener("click", makeMove); //add event listener to all squares
     }
@@ -42,16 +42,13 @@ document.addEventListener('DOMContentLoaded', function () {
     function freezeBoard() { //keep this, but add a method to freeze item after it's clicked
         for (let item of board) {
             item.removeEventListener("click", makeMove); //stop listening for clicks
-            
         }
     }
-
-
+    
     function resetBoard() { // taking away all values on board.
         notificationButton.innerText = "Turn: Player 1";
         isGoing = true;
         currentPlayer = 1;
-        console.log("reset");
         newRound();
     }
     
@@ -105,18 +102,19 @@ document.addEventListener('DOMContentLoaded', function () {
 
 function checkForWin() {
         let winner = determineWinningMoves();
+        let draw = isDraw();
         if (winner === 1) {
             notificationButton.innerHTML = "Player 1 wins!";
             startButton.innerHTML = "Play Again";
             freezeBoard();
             isGoing = false;
-            console.log("Player 1 wins!");
         } else if (winner === 2) {
             notificationButton.innerHTML = "Player 2 wins!";
             startButton.innerHTML = "Play Again";
             freezeBoard();
             isGoing = false;
-            console.log("Player 2 wins!");
+        } else if (draw) {
+            announceDraw();
         }
     }
 
@@ -138,8 +136,6 @@ function checkForWin() {
             if (currentPlayer === 1) {
                 this.src = XSrc;
                 this.style.opacity = "1"; //display x on board
-                console.log(this);
-                console.log('successful');
                 checkForWin();
                 if(isGoing) switchPlayer();//maybe switch player if and only if the game is still going
                 //can create boolean variable
@@ -147,8 +143,6 @@ function checkForWin() {
             } else {
                 this.src = OSrc;
                 this.style.opacity = "1";
-                console.log(this);
-                console.log('successful');
                 checkForWin();
                 if(isGoing) switchPlayer();
             }
