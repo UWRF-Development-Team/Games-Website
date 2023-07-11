29238f8f08e9021006e1584b12f9102f810922a3
Randy.F <randy.franzmeier@my.uwrf.edu>
2023-07-11 15:33:54 -0500

    Styled layout component

--- Tic-Tac-Toe/dev-tic-tac-toe/src/components/Layout.js
+++ Tic-Tac-Toe/dev-tic-tac-toe/src/components/Layout.js
@@ -1,16 +1,15 @@
-import './styles/Layout.css';
+import './styles/Layout.css'
 import logo from "../images/logo5.png"
+import {React} from "react"
 export default function Layout() {
-    const listTitles = [<h3> Home </h3>, <h3> Projects </h3>];
+    const listTitles = [<img id="falcon-two" src={logo} alt="Logo" />, <h3 id="title">UWRF Development Team</h3>, <h3> Home </h3>, <h3> Projects </h3>,  ];
     const listItems = listTitles.map(item => <li className="nav-items" key={item}>{item}</li>);
 
     return (
-            <div>
+            <>
             <nav>
-                <img id="falcon-two" src={logo} alt="Logo" />
-                <h3 id="title">UWRF Development Team</h3>
                 <ul>{listItems}</ul>
             </nav>
-        </div>
+        </>
     );
 }
