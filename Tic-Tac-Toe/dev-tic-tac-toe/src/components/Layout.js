eade39c3a8c575d96a61690ed3992b3cba0805f9
theoliverlear <sigwartholiver2@gmail.com>
2023-08-03 18:12:09 -0500

    Change picture
    Took 51 minutes

--- Tic-Tac-Toe/dev-tic-tac-toe/src/components/Layout.js
+++ Tic-Tac-Toe/dev-tic-tac-toe/src/components/Layout.js
@@ -1,9 +1,8 @@
 import './styles/Layout.css';
-//import logo from "../images/logo5.png";
-import logo from "../images/logo_with_title.png";
+import logo from "../images/logo_with_text_three.png";
 import {React} from "react";
 export default function Layout() {
-    const listTitles = [<img id="falcon-two" src={logo} alt="Logo" />, <h3> Home </h3>, <h3> Projects </h3>,  ];
+    const listTitles = [<img id="falcon-two" src={logo} alt="Logo" />, <h3> Home </h3>, <h3> Projects </h3>];
     const listItems = listTitles.map(item => <li className="nav-items" key={item}>{item}</li>);
 
     return (
