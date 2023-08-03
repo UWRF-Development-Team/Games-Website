b0c3a5c675ea3ebd7faa44176451ee6858952f61
theoliverlear <sigwartholiver2@gmail.com>
2023-08-03 18:12:38 -0500

    Add logic to handle button click event in server.js and display a response message.
    Took 18 seconds

--- Tic-Tac-Toe/dev-tic-tac-toe/src/server/server.js
+++ Tic-Tac-Toe/dev-tic-tac-toe/src/server/server.js
@@ -3,6 +3,7 @@ const app = express()
 app.set('view engine', 'ejs')
 
 app.get('/startButton', (req, res) => {
+    res.text("The button was clicked")
     //insert the method from vanilla js that clears the board when the button is clicked
 })
 
