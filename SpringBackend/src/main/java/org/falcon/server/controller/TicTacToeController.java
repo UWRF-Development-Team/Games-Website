e9dc975c83ef1e65c5f42efd7afd379024f51a33
theoliverlear <sigwartholiver2@gmail.com>
2023-09-12 07:40:24 -0500

    Comment code and add pseudo refresh functionality
    Took 42 seconds

--- SpringBackend/src/main/java/org/falcon/server/controller/TicTacToeController.java
+++ SpringBackend/src/main/java/org/falcon/server/controller/TicTacToeController.java
@@ -53,6 +53,75 @@ public class TicTacToeController {
         return "index";
     }
     //----------------------------Select-Spot---------------------------------
+//    @Deprecated
+//    //@RequestMapping("/tictactoe/select/{spot}")
+//    public String selectSpot(@PathVariable String spot, Model model) {
+//        //-------------------------Load-Board---------------------------------
+//        for (int i = 0; i < 9; i++) {
+//            if (this.service.getGame().getBoard().getBoard()[i] == 'X') {
+//                model.addAttribute(pieceVis[i], "opacity: 1");
+//                model.addAttribute(pieceSrc[i], "/images/X.png");
+//            } else if (this.service.getGame().getBoard().getBoard()[i] == 'O') {
+//                model.addAttribute(pieceVis[i], "opacity: 1");
+//                model.addAttribute(pieceSrc[i], "/images/O.png");
+//            } else {
+//                model.addAttribute(pieceVis[i], "opacity: 0.001");
+//            }
+//        }
+//        //-------------------------Make-Move----------------------------------
+//        int intSpot = Integer.parseInt(spot) - 1;
+//        if (this.service.getGame().getBoard().isAvailable(intSpot)) {
+//            if (this.service.getGame().getCurrentPlayer().getPlayerPiece() == 'X') {
+//                this.service.getGame().makeMove(intSpot);
+//                this.selectSpotModel("/images/X.png", intSpot, model);
+//            } else {
+//                this.service.getGame().makeMove(intSpot);
+//                this.selectSpotModel("/images/O.png", intSpot, model);
+//            }
+//        }
+//        this.service.getGame().getBoard().printBoard();
+//        return "index";
+//    }
+    //----------------------------Select-Spot---------------------------------
+//    @RequestMapping("/tictactoe/select/{spot}")
+//    public String selectSpot(@PathVariable String spot, Model model) {
+//        //-------------------------Load-Board---------------------------------
+//        for (int i = 0; i < 9; i++) {
+//            if (this.service.getGame().getBoard().getBoard()[i] == 'X') {
+//                model.addAttribute(pieceVis[i], "opacity: 1");
+//                model.addAttribute(pieceSrc[i], "/images/X.png");
+//            } else if (this.service.getGame().getBoard().getBoard()[i] == 'O') {
+//                model.addAttribute(pieceVis[i], "opacity: 1");
+//                model.addAttribute(pieceSrc[i], "/images/O.png");
+//            } else {
+//                model.addAttribute(pieceVis[i], "opacity: 0.001");
+//            }
+//        }
+//        //-------------------------Make-Move----------------------------------
+//        int intSpot = switch (spot) {
+//            case "one" -> 0;
+//            case "two" -> 1;
+//            case "three" -> 2;
+//            case "four" -> 3;
+//            case "five" -> 4;
+//            case "six" -> 5;
+//            case "seven" -> 6;
+//            case "eight" -> 7;
+//            case "nine" -> 8;
+//            default -> -1;
+//        };
+//        if (this.service.getGame().getBoard().isAvailable(intSpot)) {
+//            if (this.service.getGame().getCurrentPlayer().getPlayerPiece() == 'X') {
+//                this.service.getGame().makeMove(intSpot);
+//                this.selectSpotModel("/images/X.png", intSpot, model);
+//            } else {
+//                this.service.getGame().makeMove(intSpot);
+//                this.selectSpotModel("/images/O.png", intSpot, model);
+//            }
+//        }
+//        this.service.getGame().getBoard().printBoard();
+//        return "index";
+//    }
     @RequestMapping("/tictactoe/select/{spot}")
     public String selectSpot(@PathVariable String spot, Model model) {
         //-------------------------Load-Board---------------------------------
@@ -68,7 +137,18 @@ public class TicTacToeController {
             }
         }
         //-------------------------Make-Move----------------------------------
-        int intSpot = Integer.parseInt(spot) - 1;
+        int intSpot = switch (spot) {
+            case "one" -> 0;
+            case "two" -> 1;
+            case "three" -> 2;
+            case "four" -> 3;
+            case "five" -> 4;
+            case "six" -> 5;
+            case "seven" -> 6;
+            case "eight" -> 7;
+            case "nine" -> 8;
+            default -> -1;
+        };
         if (this.service.getGame().getBoard().isAvailable(intSpot)) {
             if (this.service.getGame().getCurrentPlayer().getPlayerPiece() == 'X') {
                 this.service.getGame().makeMove(intSpot);
@@ -79,8 +159,10 @@ public class TicTacToeController {
             }
         }
         this.service.getGame().getBoard().printBoard();
-        return "index";
+        return "boardPatch :: boardPatch";
     }
+
+    //-------------------------Select-Spot-Model------------------------------
     public void selectSpotModel(String imgPath, int spot, Model model) {
         String visAttr = "opacity: 1";
         model.addAttribute(pieceVis[spot], visAttr);
